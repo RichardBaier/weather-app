6f3bbc3eea05593048139ce87cd538d21e5940a0
SuperDork <bearbaier@gmail.com>
2022-10-22 17:00:10 -0500

    made the fetch request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
-fetch('api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={2bfce013845d4eb44cc8e46d179d989e}')
+const appid = '2bfce013845d4eb44cc8e46d179d989e'
+
+async function weatherSearch(search) {
+    const searchCity = `https://api.openweathermap.org/geo/1.0/direct?q={search}&limit=1&appid={appid}`
+    let cityWeather;
+
+    fetch(searchCity) 
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(result => {
+        cityWeather = `https://api.openweathermap.org/data/3.0/onecall?lat=${result[0].lat}&lon=${result[0].lon}&appid=${appid}&units=imperial`;
+    })
+} 

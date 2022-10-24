a2c2c04bb5493f764fc579b79f826607388394b8
SuperDork <bearbaier@gmail.com>
2022-10-23 19:31:21 -0500

    finished the fetch call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,26 @@
-const appid = '2bfce013845d4eb44cc8e46d179d989e'
+const appid = '721c8b136b03c70cf425c1cfedf9e114';
 
-async function weatherSearch(search) {
-    const searchCity = `https://api.openweathermap.org/geo/1.0/direct?q={search}&limit=1&appid={appid}`
+async function weatherSearch(searchInput) {
+    const citySearch = `https://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=1&appid=${appid}`
     let cityWeather;
 
-    fetch(searchCity) 
+    await fetch(citySearch) 
     .then(res => res.json())
     .then(result => {
         cityWeather = `https://api.openweathermap.org/data/3.0/onecall?lat=${result[0].lat}&lon=${result[0].lon}&appid=${appid}&units=imperial`;
+        console.log(result)
     })
+    await fetch(cityWeather)
+    .then(res => res.json())
+    .then(data => console.log(data))
 } 
+
+$('.searchBtn').click(() => {
+    var searchInput = $('#searchInput').val();
+
+    weatherSearch(searchInput);
+})
+
+function dataDisplay() {
+    
+}

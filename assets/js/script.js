4db275045e657a500ccfefbb4440b49ca785877b
SuperDork <bearbaier@gmail.com>
2022-10-23 23:34:14 -0500

    finished the five day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,15 @@ async function weatherSearch(searchInput) {
         cityWeather = `https://api.openweathermap.org/data/3.0/onecall?lat=${result[0].lat}&lon=${result[0].lon}&appid=${appid}&units=imperial`;
         console.log(result)
     })
-    await fetch(cityWeather)
-    .then(res => res.json())
-    .then(data => console.log(data))
+
+    if(cityWeather) {
+        await fetch(cityWeather)
+        .then(res => res.json())
+        .then(result => {
+            currentData(result, searchInput);
+            fiveData(result, searchInput);
+        })
+    }
 } 
 
 $('.searchBtn').click(() => {
@@ -21,6 +27,47 @@ $('.searchBtn').click(() => {
     weatherSearch(searchInput);
 })
 
-function dataDisplay() {
-    
+//Function to set the current weather
+function currentData(data, city) {
+    const current = data.current;
+    console.log(current);
+    const date = new Date(current.dt * 1000).toLocaleDateString('en-US');
+    //const icon = `https://openweathermap.org/img/wn/${current.weather[0].icon}.png`;
+
+    const currentHtml = `<h2>${city} (${date})</h2>
+                        <img src='https://openweathermap.org/img/wn/${current.weather[0].icon}.png'>
+                        <p>Temp: ${current.temp}°F</p>
+                        <p>Wind: ${current.wind_speed} MPH</p>
+                        <p>Humidity: ${current.humidity}%</p>`
+
+    $('#today-content').html('')
+    $('#today-content').append(currentHtml);
 }
+
+// Function to set 5 day weather
+function fiveData(data, city) {
+    const daily = data.daily;
+    console.log(daily)
+
+    $('#forecast-container').html('');
+
+    daily.forEach((day, index) => {
+        if(index < 5) {
+
+            const i = daily[index];
+
+            const date = new Date(i.dt * 1000).toLocaleDateString('en-US');
+
+            const dailyHtml = `<div class='day-forecast'>
+                                <h3>${date}</h3>
+                                <img src='https://openweathermap.org/img/wn/${i.weather[0].icon}.png'>
+                                <p>Temp: ${i.temp}°F</p>
+                                <p>Wind: ${i.wind_speed} MPH</p>
+                                <p>Humidity: ${i.humidity}%</p>
+                                </div>` 
+            
+            $('#forecast-container').append(dailyHtml);
+        }
+    });
+}
+// Function to add search to search history
